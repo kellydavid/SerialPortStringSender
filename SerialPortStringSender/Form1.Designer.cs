0aa6896b9ad6520a057b171af88bc9e3ed0fe57d
kellydavid <kellydavid178@gmail.com>
2014-08-22 16:36:00 +0100

    Added icon to application

--- SerialPortStringSender/Form1.Designer.cs
+++ SerialPortStringSender/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SerialPortStringSender));
             this.sendStringTextBox = new System.Windows.Forms.TextBox();
             this.sendButton = new System.Windows.Forms.Button();
             this.comPortComboBox = new System.Windows.Forms.ComboBox();
@@ -104,6 +105,7 @@
             this.Controls.Add(this.comPortComboBox);
             this.Controls.Add(this.sendButton);
             this.Controls.Add(this.sendStringTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SerialPortStringSender";
             this.Text = "SerialPortStringSender";
             this.ResumeLayout(false);
